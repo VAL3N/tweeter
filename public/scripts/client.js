f6e9ccf5b1e516dc8a53cbda4fb96661229b7e4d
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-07-16 04:11:07 +0000

    modified the if statement conditions to show alerts

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,30 +6,30 @@
 
 $(document).ready(function() {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ]
 
   // new code
   const renderTweets = function(data) {
@@ -94,7 +94,7 @@ $(document).ready(function() {
     
   }
 
-    renderTweets(data);
+    //renderTweets(data);
 
 
 
@@ -103,10 +103,14 @@ $(document).ready(function() {
       event.preventDefault();
 
       if ($('textarea').val().length === 0 ) {
-        alert('Your text area is empty!');
+        $('.alert-2').slideDown(1000);
+        $('.alert').slideUp(1000);
       } else if ($("textarea").val().length > 140) {
-        alert('Woah there cowboy, you\'ve gone too far!');
+        $('.alert').slideDown(1000);
+        $('.alert-2').slideUp(1000);
       } else {
+        $('.alert').slideUp(1000);
+        $('.alert-2').slideUp(1000);
       $.ajax({ 
         url: '/tweets/', 
         method: 'POST', 
